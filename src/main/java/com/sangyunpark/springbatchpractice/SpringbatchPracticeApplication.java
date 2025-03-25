659247f77e21b97ece1ea8eedb5dbe347cfb8694
sangyunpark99 <psangyun5108@gmail.com>
2025-03-25 20:34:59 +0900

    Initial commit

---
+++ src/main/java/com/sangyunpark/springbatchpractice/SpringbatchPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sangyunpark.springbatchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbatchPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sangyunpark/springbatchpractice/SpringbatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangyunpark.springbatchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
